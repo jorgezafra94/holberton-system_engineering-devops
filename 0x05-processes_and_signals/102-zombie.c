1b30e86f66b0cf947d9020277ab3625535820679
jorgezafra94 <947@holbertonschool.com>
2019-11-05 00:07:58 +0000

    zombies in C

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+/**
+ * infinite_while - infinite loop
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ *main - main function
+ *Return: Nothing
+ */
+void main(void)
+{
+	int i = 0;
+	pid_t child_pid;
+
+	for (; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			dprintf(1, "Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+}
