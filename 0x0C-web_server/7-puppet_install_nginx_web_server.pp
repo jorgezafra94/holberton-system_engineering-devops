84e8aece35e3ada80f039d540256b7a683b2783d
jorgezafra94 <947@holbertonschool.com>
2019-12-11 18:41:40 +0000

    [feature] puppet command for Nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,10 @@
 # commands in puppet in order to configure our Nginx
 
 exec {'install':
-  command  => 'sudo apt update ; sudo apt -y install nginx',
+  command  => 'sudo apt update ; sudo apt -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
-exec {'content index':
-  command  => 'echo "Holberton School" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-exec {'config in default':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default && sudo service nginx restart',
+exec {'default':
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
   provider => shell,
 }
