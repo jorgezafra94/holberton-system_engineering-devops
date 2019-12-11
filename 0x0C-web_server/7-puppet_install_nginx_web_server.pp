f98d9e48fefc98b800acb27e28e3ddc895800cca
jorgezafra94 <947@holbertonschool.com>
2019-12-11 16:01:22 +0000

    [feature] puppet command for Nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+package {'nginx':
+  ensure => 'installed',
+}
+file {'/var/www/html/index.html':
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School',
+}
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
+  notify  => Service['nginx'],
+}
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
