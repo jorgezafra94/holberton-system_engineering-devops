0dbc88a55aa52ea57754165c69850b9169f2890d
jorgezafra94 <947@holbertonschool.com>
2020-01-02 20:58:21 +0000

    [fix] adding env variable

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,12 +1,13 @@
 # commands in puppet in order to configure Nginx with HTTP header
 
 exec {'install-Nginx':
-  command  => 'sudo apt update ; sudo apt -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
+  command    => 'sudo apt update ; sudo apt -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
+    provider => shell,
+    }
 
-exec {
-  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\t# Adding Header/" /etc/nginx/nginx.conf; sudo sed -i "s/# Adding Header/# Adding Header \n\tadd_header X-Served-By $HOSTNAME;/" /etc/nginx/nginx.conf; sudo service nginx restart'
-  provider => shell,
+exec {'configure HTTP header':
+  environment  => ["name=${hostname}"],
+    command    => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\t# Adding Header/" /etc/nginx/nginx.conf; sudo sed -i "s/# Adding Header/# Adding Header \n\tadd_header X-Served-By $name;/" /etc/nginx/nginx.conf; sudo serv\
+    ice nginx restart',
+      provider => shell,
 }
-
